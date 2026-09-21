40fc0aa0a77219a238dbbcffbfcd65ea3d53f551
minh nguyene <yellowwheat@gmail.com>
2026-09-21 14:43:48 -0700

    Work on wave 1

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,17 @@
 class Vendor:
-    pass
+    def __init__(self, inventory = []):
+        self.inventory = inventory
+
+    def add(self, item):
+        if item:
+            self.inventory.append(item)
+            return item
+
+        return None
+
+    def remove(self, item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+
+        return None
