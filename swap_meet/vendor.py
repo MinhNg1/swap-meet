ef57dd054e4228daf1fb04fd36b2d76ee24c0cfc
Hardeep Kaur <hardeep1107@gmail.com>
2026-09-22 13:55:03 -0700

    Added the Logic for Wave 3

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -21,3 +21,14 @@ class Vendor:
             if item_id == item.id:
                 return item
         return None
+
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in other_vendor.inventory: 
+            return False
+
+        self.inventory.append(their_item)
+        other_vendor.inventory.append(my_item)
+        self.inventory.remove(my_item)
+        other_vendor.inventory.remove(their_item)
+
+        return True
