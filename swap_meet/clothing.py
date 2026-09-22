b31227101d42a5ef05d6e2da2a974ced0d0a54d8
Hardeep Kaur <hardeep1107@gmail.com>
2026-09-22 14:59:34 -0700

    Started on Wave 5 - Clothing class

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,2 +1,12 @@
-class Clothing:
-    pass
+from swap_meet.item import Item
+
+class Clothing(Item):
+    def __init__(self, id=None, fabric="Unknown"):
+        super().__init__(id)
+        self.fabric = fabric
+
+    def __str__(self):
+        item_str = super().str()
+        fabric_str = f"It is made from {self.fabric} fabric."
+
+        return " ".join((item_str,fabric_str))
