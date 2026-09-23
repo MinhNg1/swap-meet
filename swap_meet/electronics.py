a970aa8dcc8698e4e5891c78e7a9111c4f63d51d
minh nguyene <yellowwheat@gmail.com>
2026-09-23 15:05:56 -0700

    Work on wave 5

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,12 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+
+class Electronics(Item):
+    def __init__(self, id=None, condition=0, type="Unknown"):
+        super().__init__(id, condition)
+        self.type = type
+
+    def __str__(self):
+        item_str = super().__str__()
+        item_specific_str = f"This is a {self.type} device."
+
+        return " ".join((item_str, item_specific_str))
--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,14 +1,34 @@
 import uuid
 
 class Item:
-    def __init__(self, id=None):
+    def __init__(self, id=None, condition=0) -> None:
         if id is None:
             self.id = uuid.uuid4().int
         else:
             self.id = id
 
+        self.condition = condition
+
     def get_category(self):
         return self.__class__.__name__
+
+    def condition_description(self):
+        condition = self.condition
+        
+        if condition == 0:
+            return "Sell as is. Use as part only"
+        elif condition == 1:
+            return "fair"
+        elif condition == 2:
+            return "good"
+        elif condition == 3:
+            return "very good"
+        elif condition == 4:
+            return "exellent, like new"
+        elif condition == 5:
+            return "New, never used"
+        else:
+            return "Unknown" 
 
     def __str__(self):
         return f"An object of type {self.get_category()} with id {self.id}."
