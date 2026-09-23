69c6ff7e2de05b5d68bb529ff3e682e50a4ad16f
minh nguyene <yellowwheat@gmail.com>
2026-09-23 15:05:08 -0700

    Fix test error on Clothing

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -1,12 +1,12 @@
 from swap_meet.item import Item
 
 class Clothing(Item):
-    def __init__(self, id=None, fabric="Unknown"):
-        super().__init__(id)
+    def __init__(self, id=None, condition=0, fabric="Unknown"):
+        super().__init__(id, condition)
         self.fabric = fabric
 
     def __str__(self):
-        item_str = super().str()
-        fabric_str = f"It is made from {self.fabric} fabric."
+        item_str = super().__str__()
+        item_specific_str = f"It is made from {self.fabric} fabric."
 
-        return " ".join((item_str,fabric_str))
+        return " ".join((item_str, item_specific_str))
